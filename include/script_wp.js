7b85dd7758250a0d707ef0e36df6df61357d7124
Martin Fors <martin.fors@frostkom.se>
2018-04-07 10:25:45 +0200

    Fix bug when minifying script

--- include/script_wp.js
+++ include/script_wp.js
@@ -9,7 +9,7 @@ jQuery(function($)
 	{
 		var dom_obj = $(this),
 			dom_name = dom_obj.val(),
-			dom_url = dom_obj.parent('.form_textfield').siblings('input').val()
+			dom_url = dom_obj.parent('.form_textfield').siblings('input').val(),
 			dom_select = dom_obj.parent('.form_textfield').siblings('.form_select').children('select');
 
 		dom_select.attr('name', dom_select.attr('id') + "[" + dom_url + "|" + dom_name + "]").val('custom_name');
